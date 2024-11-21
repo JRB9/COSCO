d5e2f2659900f110ddfecbc7d6902b90978a336a
ashleygomez06 <ashley.gomez06@utrgv.edu>
2024-11-21 00:26:16 -0600

    Update TapNet.py

--- Baselines/TapNet.py
+++ Baselines/TapNet.py
@@ -6,8 +6,8 @@ from aeon.classification.deep_learning import TapNetClassifier
 from aeon.datasets import load_unit_test
 from sklearn.metrics import accuracy_score
 
-def train_tapnet(train_data, train_label, test_data, test_label, input_size):
-  tapnet = TapNetClassifier(n_epochs=nEpoch,batch_size=8)
+def train_tapnet(train_data, train_label, test_data, test_label, input_size,args):
+  tapnet = TapNetClassifier(n_epochs=args.nEpoch,batch_size=8)
   tapnet.fit(train_data, train_label)
   y_pred = tapnet.predict(test_data)
 
